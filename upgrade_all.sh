e6fc465f2704ef2320bc0ef92552b685d687ad74
Clemens <clemens@thin.unividuell.org>
2024-03-08 10:49:00 +0100

    upgrade all script

---
+++ upgrade_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# better bash; kudos: https://wizardzines.com/comics/bash-debugging/
+set -x # prints out every line ot the script as it executes, with all variables expanded
+
+# pull current images
+docker pull caddy:2 pihole/pihole
+docker pull lmscommunity/logitechmediaserver 
+docker pull giof71/squeezelite 
+docker pull ghcr.io/home-assistant/home-assistant:stable
+# remove current container
+docker rm -f caddy pihole lms squeezelite home-assistant
+# restart
+docker compose up -d
+# houeskeeping
+docker system prune --force
