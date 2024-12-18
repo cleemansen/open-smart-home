d50e6023e8b25025fa4251345ba8756a771b3dc6
Clemens <clemens@thin.unividuell.org>
2024-12-18 14:11:51 +0100

    exchange LMS by music-assistant / switch to more recent squeezlite

--- upgrade_all.sh
+++ upgrade_all.sh
@@ -6,10 +6,11 @@ set -x # prints out every line ot the script as it executes, with all variables 
 docker pull caddy:2
 docker pull pihole/pihole:latest
 docker pull lmscommunity/logitechmediaserver 
-docker pull giof71/squeezelite 
+docker pull giof71/squeezelite:daily-debian-full-squeezelite-current-sourceforge
 docker pull ghcr.io/home-assistant/home-assistant:stable
+docker pull ghcr.io/music-assistant/server:beta
 # remove current container
-docker rm -f caddy pihole lms squeezelite home-assistant
+docker rm -f caddy pihole lms squeezelite home-assistant music-assistant-server
 # restart
 docker compose up -d
 # houeskeeping
